344b2459742c51471f0f1dc58f1485529095437d
Thomas Schultz <tjschultz1@mail.mccneb.edu>
2022-08-28 11:53:06 -0500

    code changes
    code changes

--- week-3/beverage.js
+++ week-3/beverage.js
@@ -1,14 +1,12 @@
 //Add import statement for product class from product.js
-import {
-    Product
-}
-
-from "./product.js";
+import { Product } from "./product.js";
 
 
 //Create Beverage class. Export class.
-export class Beverage extends Product {
-    constructor(name, price) {
+export class Beverage extends Product
+{
+    constructor(name, price)
+    {
         //Create super function to pass parameters through parent (Product) class.
         super(name, price);
     }
