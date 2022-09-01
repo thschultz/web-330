51d60781dcbe773219f6fdf263a22b3aa4d6e8c3
Thomas Schultz <tjschultz1@mail.mccneb.edu>
2022-09-01 16:01:27 -0500

    week 4 push
    week 4 push

---
+++ week-4/food-model.js
@@ -0,0 +1,10 @@
+export class FoodModel {
+
+	//Creating constructor with fields
+    constructor(id, name, calories) {
+
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+}
